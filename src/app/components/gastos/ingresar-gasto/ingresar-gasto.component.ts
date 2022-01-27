3b44757a1eeb0c40134a9c3639711b214acff28b
teegi10 <mateoteggi16@gmail.com>
2022-01-26 21:47:26 -0300

    first commit

---
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
+
+@Component({
+  selector: 'app-ingresar-gasto',
+  templateUrl: './ingresar-gasto.component.html',
+  styleUrls: ['./ingresar-gasto.component.css']
+})
+export class IngresarGastoComponent implements OnInit {
+
+  nombreGasto: string;
+  cantidad:number;
+  formularioIncorrecto: boolean;
+  textIncorrecto: string;
+
+  constructor(private _presupuestoService: PresupuestoService) {
+    this.nombreGasto = '';
+    this.cantidad = 0;
+    this.formularioIncorrecto= false;
+    this.textIncorrecto ='NOmbre gasto o cantidad incorrecta';
+   }
+
+  ngOnInit(): void {
+  }
+
+  agregarGasto(){
+
+    if(this.cantidad > this._presupuestoService.restante){
+      this.formularioIncorrecto = true;
+      this.textIncorrecto = 'Cantidad ingresada es mayor al restante';
+      return;
+    }
+    if(this.nombreGasto === '' || this.cantidad <= 0){
+      this.formularioIncorrecto = true;
+    }else{
+
+      //Creamos el objeto
+      const GASTO = {
+        nombre: this.nombreGasto,
+        cantidad : this.cantidad
+      }
+
+
+      //Enviamos el objeto a los suscriptores via subject
+      this._presupuestoService.agregarGasto(GASTO);
+
+
+
+    //Resetamos formulario
+      this.formularioIncorrecto = false;
+      this.nombreGasto = '';
+      this.cantidad = 0;
+    }
+  }
+
+}
